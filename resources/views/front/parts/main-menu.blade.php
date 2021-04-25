91c53efa5a5376d848148b5f79bcaf3b5ba5ad7b
smatDnepr <smat.dnepr@gmail.com>
2021-04-25 17:49:14 +0300

    убрал public из url при переходе на #section_about

--- resources/views/front/parts/main-menu.blade.php
+++ resources/views/front/parts/main-menu.blade.php
@@ -3,7 +3,14 @@
 		<a href="{{ route('front.index') }}"><span>{{ __('Главная') }}</span></a>
 	</li>
 	<li>
-		<a href="#section_about"><span>{{ __('Обо мне') }}</span></a>
+		<a href="
+		@if (Route::currentRouteName() == 'front.index')
+			#section_about
+		@else
+			{{ route('front.index') }}#section_about
+		@endif">
+			<span>{{ __('Обо мне') }}</span>
+		</a>
 	</li>
 	<li>
 		<a href="#section_some_work"><span>{{ __('Поработаем?') }}</span></a>
